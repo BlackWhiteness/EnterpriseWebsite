f59a919b4892702040da0f037c6adeb5736f6dbe
GreatKing <594136672@qq.com>
2019-11-06 23:11:04 +0800

    test

--- application/search/model/Clientapiofes.php
+++ application/search/model/Clientapiofes.php
@@ -1,25 +1,29 @@
 <?php
+
 namespace app\search\model;
 
 use Elasticsearch\ClientBuilder;
 use think\Exception;
 
-class Clientapiofes {
+class Clientapiofes
+{
 
     const LOG_FILE = 'Utils_Search_ClientApiOfEs.log';
 
     private $mEs;
 
     // 构造函数
-    public function __construct() {
+    public function __construct()
+    {
         $config = array(
-            '127.0.0.1:9200'
+            '127.0.0.1:' . \env('es.port', '9200')
         );//exit;
         $this->mEs = ClientBuilder::create()->setHosts($config)->build();
     }
 
     // 创建索引
-    public function createIndex($params) { // 只能创建一次
+    public function createIndex($params)
+    { // 只能创建一次
         try {
             return $this->mEs->indices()->create($params);
         } catch (\Exception $e) {
@@ -29,8 +33,9 @@ class Clientapiofes {
     }
 
     // 删除索引
-    public function deleteIndex($params) {
-        try{
+    public function deleteIndex($params)
+    {
+        try {
             return $this->mEs->indices()->delete($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'deleteIndex', array('message:' => $e->getMessage()));
@@ -39,8 +44,9 @@ class Clientapiofes {
     }
 
     // 添加文档
-    public function addDoc($params) {
-        try{
+    public function addDoc($params)
+    {
+        try {
             return $this->mEs->index($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'addDoc', array('message:' => $e->getMessage()));
@@ -49,8 +55,9 @@ class Clientapiofes {
     }
 
     // 判断文档存在
-    public function existsDoc($params) {
-        try{
+    public function existsDoc($params)
+    {
+        try {
             return $this->mEs->exists($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'existsDoc', array('message:' => $e->getMessage()));
@@ -60,8 +67,9 @@ class Clientapiofes {
 
 
     // 获取文档
-    public function getDoc($params) {
-        try{
+    public function getDoc($params)
+    {
+        try {
             return $this->mEs->get($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'getDoc', array('message:' => $e->getMessage()));
@@ -70,8 +78,9 @@ class Clientapiofes {
     }
 
     // 建议文档
-    public function suggest($params) {
-        try{
+    public function suggest($params)
+    {
+        try {
             return $this->mEs->suggest($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'suggest', array('message:' => $e->getMessage()));
@@ -80,8 +89,9 @@ class Clientapiofes {
     }
 
     // 更新文档
-    public function updateDoc($params) {
-        try{
+    public function updateDoc($params)
+    {
+        try {
             return $this->mEs->update($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'updateDoc', array('message:' => $e->getMessage()));
@@ -91,8 +101,9 @@ class Clientapiofes {
 
 
     // 删除文档
-    public function deleteDoc($params) {
-        try{
+    public function deleteDoc($params)
+    {
+        try {
             return $this->mEs->delete($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'deleteDoc', array('message:' => $e->getMessage()));
@@ -101,9 +112,10 @@ class Clientapiofes {
     }
 
     // 查询文档 (分页，排序，权重，过滤)
-    public function search($params) {
+    public function search($params)
+    {
 
-        try{
+        try {
             return $this->mEs->search($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'search', array('message:' => $e->getMessage()));
@@ -111,8 +123,9 @@ class Clientapiofes {
         }
     }
 
-    public function bulk($params) {
-        try{
+    public function bulk($params)
+    {
+        try {
             return $this->mEs->bulk($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'bulk', array('message:' => $e->getMessage()));
@@ -120,8 +133,9 @@ class Clientapiofes {
         }
     }
 
-    public function analyze($params) {
-        try{
+    public function analyze($params)
+    {
+        try {
             return $this->mEs->indices()->analyze($params);
         } catch (Exception $e) {
             Utils_Log::err(self::LOG_FILE, 'analyze', array('message:' => $e->getMessage()));
