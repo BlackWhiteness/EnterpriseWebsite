7d91aaec663b7124a223c00fdc91b43abcc091a2
GreatKing <594136672@qq.com>
2019-11-09 22:50:38 +0800

    test

---
+++ application/format/OfficeBuildFormat.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\format;
+
+/**
+ * 写字楼格式化
+ * Class OfficeBuildFormat
+ * @package app\format
+ */
+class OfficeBuildFormat
+{
+
+    private static $instance = null;
+
+    private function __construct()
+    {
+
+    }
+
+    private function __clone()
+    {
+
+    }
+
+    /**
+     * @return self
+     */
+    public static function getInstance()
+    {
+        if (!static::$instance instanceof self) {
+            static::$instance = new self();
+        }
+        return static::$instance;
+    }
+
+    /**
+     * 格式化用于列表
+     * @param $data
+     * @return array
+     */
+    public function formatList($data)
+    {
+        $rtn = [];
+        foreach ($data as $row) {
+            $rtn[] = [
+                'id' => $row['id'],
+                'region' => $row['region'],
+                'measurearea' => $row['measurearea'],
+                'releasetime' => $row['releasetime'],
+                'plantrent' => $row['plantrent'],
+                'newolddegree' => $row['newolddegree'],
+                'name' => $row['name'],
+                'tel' => $row['tel'],
+                'detail' => $row['detail'],
+                'tag' => $row['tag'],
+                'imgs' => $row['imgs'] ? explode(',', $row['imgs'])[0] : '',
+                'buildingname' => $row['buildingname'],
+                'address' => $row['address'],
+                'managementfee' => $row['managementfee'],
+                'decoration' => $row['decoration'],
+                'city' => $row['city'],
+                'area' => $row['area'],
+                'type' => $row['type'],
+                'title' => $row['title']];
+        }
+        return $rtn;
+    }
+
+}
