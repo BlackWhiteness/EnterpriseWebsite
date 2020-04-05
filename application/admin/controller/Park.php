6386d2cc8b953c3b7d3f42bd58c7f73dab49c39f
GreatKing <594136672@qq.com>
2020-04-05 11:10:22 +0800

    'admin_park'

---
+++ application/admin/controller/Park.php
@@ -0,0 +1,181 @@
+<?php
+
+
+namespace app\admin\controller;
+
+
+use app\admin\model\City as City_Model;
+use app\admin\model\Area as Area_Model;
+use app\admin\model\ParkManage;
+use app\admin\model\ShopManage;
+use app\common\controller\Adminbase;
+use app\format\LandFormat;
+use app\format\ParkFormat;
+use app\format\ShopFormat;
+use think\Request;
+
+
+/**
+ * Class Shop
+ * @package app\admin\controller
+ */
+class Park extends Adminbase
+{
+    public $City_Model;
+    public $Area_Model;
+
+    /**
+     *
+     */
+    protected function initialize()
+    {
+        parent::initialize();
+        $this->City_Model = new City_Model();
+        $this->Area_Model = new Area_Model();
+    }
+
+
+    /**
+     * 首页
+     * @param Request $request
+     * @return mixed|\think\response\Json
+     * @throws \think\exception\DbException
+     */
+    public function index(Request $request)
+    {
+        if ($this->request->isAjax()) {
+            $result = ParkManage::order(array('id' => 'DESC'))
+                ->paginate($request->param('limit'));
+            $total = $result->total();
+            $result = array("code" => 0, "count" => $total,
+                "data" => ParkFormat::getInstance()->formatList($result->items()));
+            return json($result);
+        }
+        return $this->fetch();
+    }
+
+    /**
+     * 后台添加厂房
+     * @param Request $request
+     * @return mixed|void
+     */
+    public function add(Request $request)
+    {
+        if ($request->isPost()) {
+            $data = $request->param();
+            if (!$data) {
+                return $this->error($data);
+            }
+            $result = ParkManage::create($data);
+            if (intval($result->id)) {
+                $this->success("添加成功！", url("index"));
+            } else {
+                $this->error('添加失败！');
+            }
+        } else {
+            $result = $this->City_Model->order(array('id' => 'DESC'))->select()->toArray();
+            $citys = '';
+            foreach ($result as $r) {
+                $citys .= "<option value='" . $r["id"] . "'>  " . $r["name"] . "</option>";
+            }
+            $this->assign("citys", $citys);
+            return $this->fetch();
+        }
+    }
+
+    /**
+     *编辑后台菜单
+     * @param Request $request
+     * @return mixed|void
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function edit(Request $request)
+    {
+        if ($request->isPost()) {
+            $data = $request->param();
+            if (!isset($data['status'])) {
+                $data['status'] = 0;
+            } else {
+                $data['status'] = 1;
+            }
+            if (!$data) {
+                return $this->error($data);
+            }
+            if (ParkManage::update($data)) {
+                $this->success("编辑成功！", url("index"));
+            } else {
+                $this->error('编辑失败！');
+            }
+        } else {
+            $id = $request->param('id/d', '');
+            $rs = ParkManage::where(["id" => $id])->find();
+            $this->assign("data", $rs);
+            $result = $this->City_Model->order(array('id' => 'DESC'))->select()->toArray();
+            $citys = '';
+            foreach ($result as $r) {
+                $citys .= "<option value='" . $r["id"] . "' " . ($r['id'] == $rs['city'] ? 'checked' : '') . " >  " . $r["name"] . "</option>";
+            }
+            $result = $this->Area_Model->where(array("parentId" => $rs['city']))->order(array('id' => 'DESC'))->select()->toArray();
+            $areas = '';
+            foreach ($result as $r) {
+                $areas .= "<option value='" . $r["id"] . "' " . ($r['id'] == $rs['area'] ? 'checked' : '') . " >  " . $r["name"] . "</option>";
+            }
+            $this->assign("id", $id);
+            $this->assign("citys", $citys);
+            $this->assign("areas", $areas);
+            return $this->fetch();
+        }
+
+    }
+
+    /**
+     * 菜单删除
+     */
+    public function del()
+    {
+        $id = $this->request->param('id/d');
+        if (empty($id)) {
+            $this->error('ID错误');
+        }
+        $model = new ParkManage();
+        if ($model->del($id) !== false) {
+
+            $this->success("删除成功！");
+        } else {
+            $this->error("删除失败！");
+        }
+    }
+
+    /**
+     * 菜单排序
+     */
+    public function listorder()
+    {
+        $id = $this->request->param('id/d', 0);
+        $listorder = $this->request->param('value/d', 0);
+        $rs = ShopManage::update(['id' => $id, 'listorder' => $listorder]);
+        if ($rs) {
+            $this->success("菜单排序成功！");
+        } else {
+            $this->error("菜单排序失败！");
+        }
+    }
+
+    /**
+     * 菜单状态
+     */
+    public function setstate()
+    {
+        $id = $this->request->param('id/d');
+        empty($id) && $this->error('参数不能为空！');
+        $status = $this->request->param('status/d');
+        if (ShopManage::update(['status' => $status], ['id' => $id])) {
+            $this->success('操作成功！');
+        } else {
+            $this->error('操作失败！');
+        }
+    }
+
+}
---
+++ application/admin/model/ParkManage.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace app\admin\model;
+
+use \think\Model;
+
+
+/**
+ * 招商模型类
+ *
+ * @package app\admin\model
+ */
+class ParkManage extends Model
+{
+    protected $pk = 'id';
+    protected $table = 'search_park';
+
+    protected $type = [
+        'imgs' => 'serialize',
+    ];
+
+    /**
+     * 对应一个城市
+     * @return \think\model\relation\BelongsTo
+     */
+    public function belongsToOneCity()
+    {
+        return $this->belongsTo('City', 'city', 'id');
+    }
+
+    /**
+     * 对应一个地区
+     * @return \think\model\relation\BelongsTo
+     */
+    public function belongsToOneArea()
+    {
+        return $this->belongsTo('Area', 'area', 'id');
+    }
+
+
+    public function del($id)
+    {
+        $id = (int)$id;
+        if (empty($id)) {
+            $this->error = '请指定需要删除的ID！';
+            return false;
+        }
+        if (false !== $this->where(array('id' => $id))->delete()) {
+            return true;
+        } else {
+            $this->error = '删除失败！';
+            return false;
+        }
+    }
+
+    /**
+     * 通用过滤
+     * @return \think\db\Query
+     */
+    public function filterCommon()
+    {
+        $query = self::getModel();
+        $city = request()->param('city');
+        if (!empty($city)) {
+            $query = $query->where('city', '=', $city);
+        }
+        $area = request()->param('area');
+        if (!empty($area)) {
+            $query = $query->where('area', '=', $area);
+        }
+
+        $query = $query->order(array('releasetime' => 'DESC'));
+
+        return $query;
+    }
+
+    /**
+     * 获取列表
+     *
+     * @return \think\db\Query|\think\Paginator
+     * @throws \think\exception\DbException
+     */
+    public function getList()
+    {
+        $query = $this->filterCommon();
+        $query = $query->paginate(request()->param('per_page',20));
+        return $query;
+    }
+
+    /**
+     * 第一页
+     *
+     * @param $city
+     * @return array|\PDOStatement|string|\think\Collection
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getFirstPage($city)
+    {
+        $list = self::where('city', '=', $city)
+            ->order('releasetime', 'desc')
+            ->page(1, 5)
+            ->select();
+        return $list;
+    }
+
+}
