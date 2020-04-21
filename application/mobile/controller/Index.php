f67e0f45201c98fece455febd7dc723027f9d9c7
GreatKing <594136672@qq.com>
2020-04-21 22:47:09 +0800

    'test'

--- application/mobile/controller/Index.php
+++ application/mobile/controller/Index.php
@@ -2,6 +2,7 @@
 
 namespace app\mobile\controller;
 
+use app\admin\model\AdManage;
 use app\admin\model\OwnerDemand;
 use app\admin\model\Workshop as Workshop_Model;
 use app\admin\model\Officebuilding;
@@ -36,11 +37,24 @@ class Index extends MobileBase
         $workshop = Workshop_Model::order(array('releasetime' => 'DESC'))->page(1, 5)->select()->toArray();
         $office = Officebuilding::where(array('type' => 2))->order(array('releasetime' => 'DESC'))->page(1, 4)->select()->toArray();
         $land = Workshop_Model::where(array('type' => 2))->order(array('releasetime' => 'DESC'))->page(1, 6)->select()->toArray();
+        $ad = AdManage::where('is_enable', '=', 1)
+            ->where('code','=','004')
+            ->order(['sort' => 'desc'])
+            ->select();
+        $adList = [];
+        foreach ($ad as $row) {
+            $adList[] = [
+                'title' => $row->title,
+                'pic_path' => $row->pic_path,
+                'href' => $row->href
+            ];
+        }
         $this->assign([
             'currentCity' =>$cityInfo,
             'workshop'=>$workshopInstance->formatList($workshop),
             'office' => $officeInstance->formatList($office),
             'land' => $landInstance->formatList($land),
+            'adList' =>$adList
         ]);
         return $this->fetch();
     }
