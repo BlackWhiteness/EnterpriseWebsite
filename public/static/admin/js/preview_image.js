04ea88e18fc216499b3785e137ff8df33b6bc352
GreatKing <594136672@qq.com>
2020-05-10 23:22:26 +0800

    'test'

--- public/static/admin/js/preview_image.js
+++ public/static/admin/js/preview_image.js
@@ -9,3 +9,13 @@ function splitImagePre(name) {
 function delImage(obj) {
     obj.parentNode.remove();
 }
+
+function splitVideoPre() {
+    let html = '';
+    html += '<div class="img-pre"><span class="close" onclick="delImage(this)">&times;</span><video src="';
+    html += res.path;
+    html += '" controls  width="50%" height="50%"></video><input type="hidden" name="video" value="';
+    html += res.path + '"></div>';
+    return html;
+
+}
